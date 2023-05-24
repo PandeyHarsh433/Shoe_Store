cc5fc890a68f00a897fe8473b66d0223c52e07dc
Harsh Pandey <pandeyharsh704@gmail.com>
2023-05-24 08:53:41 +0530

    First commit

---
+++ components/Menu.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import Link from "next/link";
+import { BsChevronDown } from "react-icons/bs";
+
+const Menu = ({ showCatMenu, setShowCatMenu }) => {
+  const data = [
+    {
+      id: 1,
+      name: "Home",
+      url: "/",
+    },
+    {
+      id: 2,
+      name: "About",
+      url: "/about",
+    },
+    {
+      id: 3,
+      name: "Categories",
+      subMenu: true,
+    },
+    {
+      id: 4,
+      name: "Contact",
+      url: "/contact",
+    },
+  ];
+
+  const subMenuData = [
+    {
+      id: 1,
+      name: "Jordan",
+      doc_count: 11,
+      link: "jordan",
+    },
+    {
+      id: 2,
+      name: "Sneakers",
+      doc_count: 8,
+      link: "sneakers",
+    },
+    {
+      id: 3,
+      name: "Running Shoes",
+      doc_count: 64,
+      link: "runningShoes",
+    },
+    {
+      id: 4,
+      name: "Football shoes",
+      doc_count: 107,
+      link: "footballShoes",
+    },
+  ];
+  return (
+    <ul className="hidden md:flex items-center gap-8 font-medium text-black">
+      {data.map((item) => (
+        <React.Fragment key={item.id}>
+          {!!item?.subMenu ? (
+            <li
+              className="cursor-pointer  flex items-center gap-2 relative"
+              onMouseEnter={() => setShowCatMenu(true)}
+              onMouseLeave={() => setShowCatMenu(false)}
+            >
+              {item.name}
+              <BsChevronDown />
+              {showCatMenu && (
+                <ul className="bg-white absolute top-6 left-0 min-w-[250px] px-1 py-1text-black shadow-lg">
+                  {subMenuData.map((submenu) => (
+                    <Link
+                      href={`/category/${submenu.link}`}
+                      key={submenu.id}
+                      onClick={() => setShowCatMenu(false)}
+                    >
+                      <li className="h-12 flex justify-between items-center px-3 hover:bg-black/[0.03] rounded-md">
+                        {submenu.name}
+                        <span className="opacity-50 text-sm">
+                          {submenu.doc_count}
+                        </span>
+                      </li>
+                    </Link>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ) : (
+            <li className="cursor-pointer">
+              <Link href={item?.url}>{item.name}</Link>
+            </li>
+          )}
+        </React.Fragment>
+      ))}
+    </ul>
+  );
+};
+
+export default Menu;
